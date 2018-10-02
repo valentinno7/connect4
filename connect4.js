6af114a77662307f0730db6c71fcf785495af4f7
Ešvovičius Valentinas <valentinas.esvovicius.2@mif.stud.vu.lt>
2018-10-02 12:21:19 +0100

    Grid

---
+++ connect4.js
@@ -0,0 +1,25 @@
+class Connect4 {
+	constructor(selector) {
+		this.ROWS=6;
+		this.COLS=7;
+		this.selector=selector;
+		this.createGrid();
+	}
+	
+	createGrid() {
+		const $board = $(this.selector);
+		console.log($board);
+		for (let row = 0; row < this.ROWS; row++) {
+			const $row = $('<div>')
+			.addClass('row');
+				$board.append($row);
+			for (let col = 0; col < this.COLS; col++) {
+				const $col = $('<div>')
+				.addClass('col empty')
+				$row.append($col);
+			}
+			$board.append($row);
+		}
+			
+	}
+}
---
+++ main.js
@@ -0,0 +1,5 @@
+$(document).ready(function() {
+  const connect4 = new Connect4('#connect4')
+  
+  console.log("Test");
+});
