1c13ac25a0755deb3b9f0dec5738871446894bb2
Ešvovičius Valentinas <valentinas.esvovicius.2@mif.stud.vu.lt>
2018-10-17 21:35:16 +0100

    Bug fixes

--- connect4.js
+++ connect4.js
@@ -2,22 +2,24 @@ class Connect4 {
   constructor(selector, rows, columns, player1, player2, whostart) {
     this.ROWS = rows;
     this.COLS = columns;
-    this.player1 = player1;
-	this.player2=player2;
-	console.log('test', this.player1, this.player2);
+    this.playername= player1;
+	//this.playername2=player2;
+	this.player='red';
+	//console.log('test', this.player, this.player2);
+	//console.log(typeof player1);
     this.selector = selector;
     this.isGameOver = false;
     this.onPlayerMove = function() {};
-    this.createGrid(this.player1);
-    this.setupEventListeners(this.player1, this.player2);
+    this.createGrid(this.playername);
+    this.setupEventListeners(player1, player2);
   }
 
   createGrid(player1) {
     const $board = $(this.selector);
     $board.empty();
+	//this.player=player1;
     this.isGameOver = false;
-    this.player = player1;
-	console.log('test', this.player);
+	//console.log('test', this.player);
     for (let row = 0; row < this.ROWS; row++) {
       const $row = $('<div>')
         .addClass('row');
@@ -35,7 +37,7 @@ class Connect4 {
   setupEventListeners(player1, player2) {
     const $board = $(this.selector);
     const that = this;
-
+	console.log('test', that.player);
     function findLastEmptyCell(col) {
       const cells = $(`.col[data-col='${col}']`);
       for (let i = cells.length - 1; i >= 0; i--) {
@@ -72,12 +74,24 @@ class Connect4 {
       )
       if (winner) {
         that.isGameOver = true;
-        alert(`Game Over! Player ${that.player} has won!`);
+        alert(`Game Over! Player ${that.playername} has won!`);
         $('.col.empty').removeClass('empty');
         return;
       }
-
-      that.player = (that.player === 'red') ? 'black' : 'red';
+	  console.log('test', that.player, that.player2);
+      //that.player = (that.player === String(that.player)) ? String(that.player2) : String(that.player);
+	  if(String(that.playername)===player1) {
+		 that.player='black';
+		 that.playername=player2;
+		 console.log('test1',player1, player2, that.player);
+		 console.log(typeof that.player, 'tipas');
+	  }
+	 else { 
+		 
+		 that.player='red';
+		 that.playername=player1;
+		 console.log('test2', player1, that.player);
+	 }
       that.onPlayerMove();
       $(this).trigger('mouseenter');
     });
