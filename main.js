a1926bfc1cdb6b1999adcfcd3b6a784e2e1e7d7c
Ešvovičius Valentinas <valentinas.esvovicius.2@mif.stud.vu.lt>
2018-11-22 23:07:36 +0000

    API usage for sign up and sign in

--- main.js
+++ main.js
@@ -5,6 +5,8 @@ document.addEventListener("DOMContentLoaded", function() {
 				if(!confirm("If you want your results to be stored sign in or sign up!"))
 				return;
 			} 
+			if(document.getElementById('player2_name').checked==true)
+				document.getElementById('player2_name').value="Computer";
 			var connect4=new Connect4('#connect4', document.getElementById('rows').value, document.getElementById('columns').value, 
 										document.getElementById('player1_name').value, document.getElementById('player2_name').value,
 										document.getElementById('firstToStart').value, document.getElementById('level').value);
@@ -39,13 +41,49 @@ document.addEventListener("DOMContentLoaded", function() {
 	}
 	
 	function signIn(){
-		var name=document.getElementById('login');
-		document.getElementById('player1_name').value=name.value;
-		document.getElementById('player1_name').readOnly=true;
+		const url = "http://twserver.alunos.dcc.fc.up.pt:8008/register";
+		var data = JSON.stringify({"nick":document.getElementById('login').value, "pass":document.getElementById('pass').value});
+		postData(url,data);
 		off();
 		event.preventDefault();
 	}
 	
+	function signUp() {
+		let login=document.getElementById('login').value;
+		let pass=document.getElementById('pass').value;
+		off();
+		console.log(login, pass);
+		const url = "http://twserver.alunos.dcc.fc.up.pt:8008/register";
+		var data = JSON.stringify({"nick":login, "pass":pass});
+		console.log(data);
+		postData(url, data);
+	}
+	
+	function postData(url, data) {
+		const xhr = new XMLHttpRequest();
+		xhr.open("POST", url, true);
+		xhr.send(data);
+		xhr.onreadystatechange=function() {
+			if(xhr.readyState===4){
+				if(xhr.status==200)
+					auth(xhr.responseText);	
+				if(xhr.status==400)
+					auth(null);
+			}
+		}
+	}
+	
+	function auth(response) {
+		var res=JSON.parse(response);
+		if(res==null){
+			alert("Bad login or password!");
+			return;
+		}
+		if(Object.keys(res).length === 0 && res.constructor === Object){
+			document.getElementById('player1_name').value=document.getElementById('login').value;
+			document.getElementById('player1_name').readOnly=true;
+		}
+	}
 	function showResults(){
 		document.getElementById("ratingsList").innerHTML='';
 		var temp='';
@@ -62,6 +100,9 @@ document.addEventListener("DOMContentLoaded", function() {
 			}
 			event.preventDefault(); 
 	}
+	
+	
+
 	
 	
 	
